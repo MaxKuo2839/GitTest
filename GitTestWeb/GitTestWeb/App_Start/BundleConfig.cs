0602f815320d789f719e1075fd9c13bb5d3ba0ef
Max Kuo <MaxKuo@wistronits.com>
2018-10-04 14:45:56 +0800

    'eeeeee'

--- GitTestWeb/GitTestWeb/App_Start/BundleConfig.cs
+++ GitTestWeb/GitTestWeb/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace GitTestWeb
         // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            int sad = 5;
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
